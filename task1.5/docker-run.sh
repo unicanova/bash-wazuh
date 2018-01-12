c97f044d401b32e3c7cf3e73d1011fbeb4cd9541
Bogdan <metallica127@mail.ru>
2018-01-12 19:40:35 +0200

    wazuh stuff

--- task1.5/docker-run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d --name elk --security-opt seccomp=unconfined --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro -t solita/ubuntu-systemd
-
-docker cp elk_install.sh elk:/tmp/
-
